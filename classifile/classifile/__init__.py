2d40e2bcb3bc653251842e01bb2b705a5ddc0cee
rawyhany <rawyhany@gmail.com>
2023-04-10 14:48:07 +0200

    Add new feature to dashboard page

---
+++ classifile/classifile/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail, Message
+
+
+
+app = Flask(__name__)
+mail = Mail(app)
+app.config['SECRET_KEY'] = '3dce62013b890416bcb508b1210ad0a1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///example.db'
+app.config['ALLOWED_EXTENSIONS'] = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOADED_IMAGES_DEST'] = 'static/media_dest'
+app.config['UPLOADED_FILES_ALLOW'] = True
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from classifile import routes
