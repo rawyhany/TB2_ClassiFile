1e125b98fd589a1745078ec24f4f63abdafcffdc
rawyhany <rawyhany@gmail.com>
2023-05-23 17:23:00 +0200

    File Relocation

--- classifile/__init__.py
+++ classifile/__init__.py
@@ -1,11 +1,9 @@
-import os
 from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail, Message
 import json
-
 
 
 
@@ -22,4 +20,4 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-
+from classifile import routes
