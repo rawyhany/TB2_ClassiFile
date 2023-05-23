e81bab83c9fb6e8d987b1e94eb46a780a3f60a30
rawyhany <rawyhany@gmail.com>
2023-05-23 18:31:13 +0200

    Forms.py Update

--- classifile/forms.py
+++ classifile/forms.py
@@ -4,6 +4,7 @@ from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from classifile.models import User
+#
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
